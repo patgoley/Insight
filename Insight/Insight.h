262824ab50eda09cea0ae7e1af0515c9484630d9
Patrick Goley <patrick.goley@gmail.com>
2015-11-11 10:31:51 -0600

    Initial Commit

---
+++ Insight/Insight.h
@@ -0,0 +1,19 @@
+//
+//  Insight.h
+//  Insight
+//
+//  Created by Patrick Goley on 11/11/15.
+//  Copyright © 2015 Affirmative. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Insight.
+FOUNDATION_EXPORT double InsightVersionNumber;
+
+//! Project version string for Insight.
+FOUNDATION_EXPORT const unsigned char InsightVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Insight/PublicHeader.h>
+
+
